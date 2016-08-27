297ce8b3c3096447dae6a381b9268a14a59e69e9
yashasvi prakash <yashasvi prakash>
2016-08-27 18:10:47 +0530

    chat.js refreshed pc

--- chatapp/public/chat.js
+++ chatapp/public/chat.js
@@ -9,14 +9,13 @@ window.onload = function() {
 
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
                 html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
-			content.scrollTop = content.scrollHeight;
         } else {
             console.log("There is a problem:", data);
         }
@@ -27,9 +26,7 @@ window.onload = function() {
             alert("Please type your name!");
         } else {
             var text = field.value;
-	    var username1 = name.value;
-            socket.emit('send', { message: text, username: username1});
-	    field.value = "";
+            socket.emit('send', { message: text, username: name.value });
         }
     };
 
