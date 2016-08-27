ce3a65fed1a03cb5ec83e412999465a177575a9c
yashasvi prakash <yashasvi prakash>
2016-08-27 14:26:09 +0530

    minimal front end and socket.js added

--- chatapp/index.js
+++ chatapp/index.js
@@ -2,9 +2,19 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
 app.get("/", function(req,res) {
-	res.send("It Works!");
+	res.render("page");
 });
 
-app.listen(port);
+app.use(express.static(__dirname + '/public'));
+var io = require('socket.io').listen(app.listen(port));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
 console.log("Listening on port "+ port);
